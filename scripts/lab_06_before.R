49210e6ccd780b38a5e939e88cdb967818d11215
Stas <masuta92@gmail.com>
2019-09-30 19:12:21 +0300

    week6
    all labs and 3 first R exercise

---
+++ scripts/lab_06_before.R
@@ -0,0 +1,22 @@
+# Esli russkie bukvi prevratilitis v krakozyabry,
+# to File - Reopen with encoding... - UTF-8 - Set as default - OK
+
+library("lubridate") # работа с датами
+
+library("sandwich") # vcovHC, vcovHAC
+library("lmtest") # тесты
+library("car") # еще тесты
+library("zoo") # временные ряды
+library("xts") # еще ряды
+library("dplyr") # манипуляции с данными
+library("broom") # манипуляции
+library("ggplot2") # графики
+
+# install.packages("rusquant",repos="http://r-forge.r-project.org", type="source")
+
+library("quantmod") # загрузка с finance.google.com
+library("rusquant") # загрузка с finam.ru
+library("sophisthse") # загрузка с sophist.hse.ru
+library("Quandl") # загрузка с Quandl
+
+
